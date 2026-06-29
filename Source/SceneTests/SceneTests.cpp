2d9fe3dbcbcdae18d08846f7049b0893f7d7597f
Michael-Evangelos Diamantis <michaeldiamantis17@gmail.com>
2026-06-29 12:46:41 +0300

    Initial commit

---
+++ Source/SceneTests/SceneTests.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SceneTests.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SceneTests, "SceneTests" );
