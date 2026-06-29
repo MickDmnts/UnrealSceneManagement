7ee7ca068cc58d55cb929bc5cf97cfb739247e63
Michael-Evangelos Diamantis <michaeldiamantis17@gmail.com>
2026-06-29 14:06:31 +0300

    Creating a character controller

--- Source/SceneTests.Target.cs
+++ Source/SceneTests.Target.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
@@ -9,7 +9,7 @@ public class SceneTestsTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V7;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
-		ExtraModuleNames.Add("SceneTests");
+
+		ExtraModuleNames.AddRange( new string[] { "SceneTests" } );
 	}
 }
--- Source/SceneTestsEditor.Target.cs
+++ Source/SceneTestsEditor.Target.cs
@@ -1,15 +1,15 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class SceneTestsEditorTarget : TargetRules
 {
-	public SceneTestsEditorTarget( TargetInfo Target) : base(Target)
+	public SceneTestsEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V7;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
-		ExtraModuleNames.Add("SceneTests");
+
+		ExtraModuleNames.AddRange( new string[] { "SceneTests" } );
 	}
 }
